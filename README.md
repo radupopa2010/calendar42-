de917ea33042961db9d4102e2cf98642745abd03
Radu Gabriel Popa <radupopa2010@yahoo.com>
2016-11-12 20:07:28 +0100

    wip

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-Solution for the calendar24 assignment.
+# calendar42- Solution for the calendar24 assignment.
 
 I will use Flask.# calendar42-
+
+_A. Get the event details (including title)_
+```
+curl --request GET \
+--header "Accept: application/json" \
+--header "Content-type: application/json" \
+--header "Authorization: Token $TOKEN" \
+"https://demo.calendar42.com/api/v2/events/$EVENT_ID/" 
--- app.py
+++ app.py
@@ -3,16 +3,33 @@
 # import the Flask class from the flask package
 from flask import Flask
 
-# create the application object
+
+# Config.
+TOKEN = 'demo+7832657@calendar42.com/0fde9f26fe8bf272cbc1336218999b620abe98b8/'
+EVENT_ID = '1d2e3220f23ec79c8b547302d1deabe9_14770730218531'
+
+
+# Create the application object.
 app = Flask(__name__)
 
 
-# use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!, radu "
+
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route('/events-with-subscriptions/<EVENT_ID>/')
+def events_with_subscriptions(EVENT_ID):
+    """Func docs here."""
+    return EVENT_ID
 
 
 # start the development server using the run() method
