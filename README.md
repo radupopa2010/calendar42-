d5a83e53405105a20c83e4b8fd2b7ed39a8647f5
Radu Gabriel Popa <radupopa2010@yahoo.com>
2016-11-12 23:51:38 +0100

    wip

--- README.md
+++ README.md
@@ -2,10 +2,34 @@
 
 I will use Flask.# calendar42-
 
+Requirement.
 _A. Get the event details (including title)_
 ```
+
 curl --request GET \
 --header "Accept: application/json" \
 --header "Content-type: application/json" \
 --header "Authorization: Token $TOKEN" \
 "https://demo.calendar42.com/api/v2/events/$EVENT_ID/" 
+
+Test.
+
+TOKEN = '0fde9f26fe8bf272cbc1336218999b620abe98b8'
+EVENT_ID = '1d2e3220f23ec79c8b547302d1deabe9_14770730218531'
+
+curl --request GET \
+--header "Accept: application/json" \
+--header "Content-type: application/json" \
+--header "Authorization: Token 0fde9f26fe8bf272cbc1336218999b620abe98b8" \
+"https://demo.calendar42.com/api/v2/events/1d2e3220f23ec79c8b547302d1deabe9_14770730218531/"
+
+
+_B. Get the event subscriptions (participants including the name)_
+```
+
+curl --request GET \
+--header "Accept: application/json" \   
+--header "Content-type: application/json" \
+--header "Authorization: Token 0fde9f26fe8bf272cbc1336218999b620abe98b8" \
+--globoff \
+"https://demo.calendar42.com/api/v2/event-subscriptions/?event_ids=[1d2e3220f23ec79c8b547302d1deabe9_14770730218531]"
--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# ---- Flask Hello World ---- #
+# ---- Calendar 24 assignment ---- #
+
+
+import requests
+from pprint import pformat
 
 # import the Flask class from the flask package
 from flask import Flask
 
 
 # Config.
-TOKEN = 'demo+7832657@calendar42.com/0fde9f26fe8bf272cbc1336218999b620abe98b8/'
+TOKEN = 'Token 0fde9f26fe8bf272cbc1336218999b620abe98b8'
 EVENT_ID = '1d2e3220f23ec79c8b547302d1deabe9_14770730218531'
 
 
@@ -14,22 +18,88 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/hello")
-# define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!, radu "
+    return "Hello, World! "
+
+
+def get_event_subscriptions(event_id):
+    """Helper func for "events_with_subscriptions" ."""
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'Authorization': TOKEN
+    }
+
+    http_url = 'https://demo.calendar42.com/api/v2/event-subscriptions/?event_ids=[%s]' % event_id
+    try:
+        resp = requests.get(http_url, headers=headers)
+    except:
+        print('GET Request to "Get the event details" FAILED ')
+        raise
+    else:
+        print(resp.status_code)
+        # import pdb; pdb.set_trace()
+        if resp.status_code == 200:
+            event_det = resp.json().get('data')
+            return event_det
+        else:
+            raise ValueError('No data')
+
+
+def get_event_details(event_id):
+    """Helper func for "events_with_subscriptions". """
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+        'Authorization': TOKEN
+    }
+
+    http_url = 'https://demo.calendar42.com/api/v2/events/' + event_id
+    try:
+        resp = requests.get(http_url, headers=headers)
+    except:
+        print('GET Request to "Get the event details" FAILED ')
+        raise
+    else:
+        print(resp.status_code)
+        # import pdb; pdb.set_trace()
+        if resp.status_code == 200:
+            event_det = resp.json().get('data')
+            if len(event_det) == 1:
+                return event_det[0]
+            else:
+                raise ValueError('Too much data')
+        else:
+            raise ValueError('No data')
 
 
 # dynamic route
-@app.route("/test/<search_query>")
-def search(search_query):
-    return search_query
-
-
-@app.route('/events-with-subscriptions/<EVENT_ID>/')
-def events_with_subscriptions(EVENT_ID):
+@app.route('/events-with-subscriptions/<event_id>/')
+def events_with_subscriptions(event_id):
     """Func docs here."""
-    return EVENT_ID
+
+    try:
+        event_details = get_event_details(event_id)
+    except ValueError:
+        print('Error while getting EVENT DETAILS: ', event_id)
+        # TODO: see what to retrn if no data is found.
+        return
+    else:
+        # Do stuff with data
+        print(pformat(event_details))
+        _id = event_details.get('id')
+        title = event_details.get('title')
+
+    try:
+        event_subs = get_event_subscriptions(event_id)
+    except ValueError:
+        print('Error while getting EVENT SUBSCRIPTIONS for event ', event_id)
+    else:
+        names = event_subs
+
+    return 'id = %s , title = %s \n\n %s' % (_id, title, names)
 
 
 # start the development server using the run() method
